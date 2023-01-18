3f9e69541a9607913c7693db1c2822053cd4cd38
gditzel <gonza.ditzel5@gmail.com>
2023-01-18 15:44:38 -0300

    changes-about-component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import "../../index.css";
 const Navbar = () => {
   return (
     <header>
-      <nav className="bg-[#31313b] lg:mt-28 lg:w-20 lg:rounded-xl">
+      <nav className="bg-[#31313b] font-poppins lg:mt-28 lg:w-20 lg:rounded-xl">
         <ul className="flex justify-evenly lg:inline">
           {links.map(({ label, route, icon }) => (
             <li key={label} className="py-2 text-center">
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+        fontFamily: {
+          'poppins': ['Poppins', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
