a58eab972361ad49d9b716aec36d9151c18a1276
gditzel <gonza.ditzel5@gmail.com>
2023-01-18 17:03:43 -0300

    update-profile-component

--- src/layout/ProfileLayout.jsx
+++ src/layout/ProfileLayout.jsx
@@ -2,15 +2,16 @@ import "../index.css";
 
 import banner from "../assests/imgs/banner.jpg";
 import profile from "../assests/imgs/Profile.jpg";
+import { SocialIcon } from "react-social-icons";
 
 const ProfileLayout = () => {
   return (
-    <section className="h-screen font-poppins lg:w-96 lg:pt-20">
+    <section className="h-screen font-poppins lg:w-[500px] lg:pt-20">
       <div>
         <img
           src={banner}
           alt="Banner del perfil"
-          className="h-80 w-full object-cover"
+          className="h-80 w-full object-cover lg:rounded-t-xl"
         />
       </div>
       <div className="h-64 -skew-y-12 bg-[#31313b]"></div>
@@ -24,16 +25,39 @@ const ProfileLayout = () => {
         <div className="mt-4 text-center text-white">
           <h1 className="text-3xl">Gonzalo Ditzel</h1>
           <h3 className="text mt-2 text-orange-500">Web Development</h3>
-          <p className="mt-4">Redes</p>
+          <div className="mt-2 flex justify-center gap-4">
+            <SocialIcon
+              url="https://www.linkedin.com/in/gonzaloditzel/"
+              bgColor="transparent"
+              fgColor="white"
+              target="_blank"
+              rel="noopener noreferrer"
+            />
+            <SocialIcon
+              url="https://github.com/gditzel"
+              bgColor="transparent"
+              fgColor="white"
+              target="_blank"
+              rel="noopener noreferrer"
+            />
+            <a href="mailto:gonza.ditzel5@gmail.com">
+              <SocialIcon
+                bgColor="transparent"
+                fgColor="white"
+                target="_blank"
+                rel="noopener noreferrer"
+              />
+            </a>
+          </div>
         </div>
-        <div className="mt-12 h-28 bg-[#31313b] text-center text-white">
+        <div className="mt-12 h-28 bg-[#31313b] text-center text-white lg:rounded-b-xl">
           <div className="line"></div>
-          <button className="mr-6 mt-7 py-2 px-4 uppercase">
+          <button className="mr-6 mt-7 py-2 px-4 uppercase hover:text-orange-500">
             <a href="#das" download="Gonzalo Ditzel CV">
               Download cv
             </a>
           </button>
-          <button className="py-2 px-4 uppercase">
+          <button className="py-2 px-4 uppercase hover:text-orange-500">
             <a href="#Contact">Contact me</a>
           </button>
         </div>
--- src/page/about/About.jsx
+++ src/page/about/About.jsx
@@ -2,11 +2,11 @@ import "../../index.css";
 
 const About = () => {
   return (
-    <section className="absolute top-[700px] rounded-r-xl bg-[#31313b] font-poppins lg:relative lg:top-0 lg:mt-28 lg:h-[600px] lg:w-[700px]">
+    <section className="fade-in absolute top-[700px] bg-[#31313b] font-poppins lg:relative lg:top-0 lg:mt-28 lg:h-[600px] lg:w-[700px] lg:rounded-r-xl">
       <div>
         <div className="circle absolute h-7 w-7 translate-y-6 translate-x-3 rounded-full opacity-50"></div>
         <h1 className="py-4 pl-6 text-3xl text-orange-500">
-          About <span className="text-white">Me</span>
+          A<span className="text-white">bout Me</span>
         </h1>
         <div className="line"></div>
       </div>
